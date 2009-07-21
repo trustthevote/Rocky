b322943444c6be6238ef374552db402ffed71d6f
Josh Susser & Jacob Maine <ops+josh+jacob@pivotallabs.com>
2009-07-21 14:31:14 -0700

    extract flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,27 @@
 module ApplicationHelper
+
+  # flash_messages
+  # use to display specified flash messages
+  # defaults to standard set: [:success, :message, :warning]
+  # example:
+  #   <%= flash_messages %>
+  # example with other keys:
+  #   <%= flash_messages :notice, :violation %>
+  # renders like:
+  #  <ul class="flash">
+  #   <li class="flash-success">Positive - successful action</li>
+  #   <li class="flash-message">Neutral - reminders, status</li>
+  #   <li class="flash-warning">Negative - error, unsuccessful action</li>
+  #  </ul>
+  def flash_messages(*keys)
+    keys = [:success, :message, :warning] if keys.empty?
+    messages = []
+    keys.each do |key|
+      messages << content_tag(:li, flash[key], :class => "flash-#{key}") if flash[key]
+    end
+    content_tag(:ul, messages.join("\n"), :class => "flash")
+  end
+
   def clippy(text, bgcolor='#FFFFFF')
     html = <<-HTML
       <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
