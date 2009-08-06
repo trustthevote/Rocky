e0076a9c8940d9932d0645876e0988fd1075e460
Josh Susser & Jacob Maine <ops+jsusser+jacob@pivotallabs.com>
2009-08-06 16:57:53 -0700

    fix from address

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
 
   def password_reset_instructions(partner)
     subject  "Password Reset Instructions"
-    from "Rock The Vote"
+    from "rtv@example.com" # FIXME: use real email address
     recipients partner.email
     sent_on Time.now.to_s(:db)
     body :url => edit_password_reset_url(:id => partner.perishable_token)
