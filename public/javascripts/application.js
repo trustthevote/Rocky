02c11fbfd776e540e57ca80b0ec462b5271b5206
Alex Lobascio & Jacob Maine <ops+bosh+jacob@pivotallabs.com>
2009-08-07 14:47:02 -0700

    give registration flow its own layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -32,15 +32,3 @@ function addTooltips(selector, target_corner, tooltip_corner) {
     show: { effect: { length: 50 } }
   })
 };
-
-$(document).ready(function() {
-  $.fn.qtip.styles.registrant = {
-    name: 'blue',
-    width: { max: '200px' },
-    color: '#006',
-    border: { width: 3, radius: 8 } // Rounded corners
-  };
-
-  addTooltips('.flat img.tooltip, .checkbox img.tooltip', 'topRight', 'bottomLeft');
-  addTooltips('legend img.tooltip', 'rightMiddle', 'leftBottom');
-});
