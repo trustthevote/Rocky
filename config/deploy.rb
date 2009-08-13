c3ce982aa27d87efd32b6ec4e576c7a43d127a8c
Josh Susser & Rob Olson <ops+jsusser+rolson@pivotallabs.com>
2009-08-13 14:40:44 -0700

    capify project

---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+set :application, "rocky"
+set :repository,  "git@github.com:pivotal/rocky.git"
+
+# If you have previously been relying upon the code to start, stop 
+# and restart your mongrel application, or if you rely on the database
+# migration code, please uncomment the lines you require below
+
+# If you are deploying a rails app you probably need these:
+
+# load 'ext/rails-database-migrations.rb'
+# load 'ext/rails-shared-directories.rb'
+
+# There are also new utility libaries shipped with the core these 
+# include the following, please see individual files for more
+# documentation, or run `cap -vT` with the following lines commented
+# out to see what they make available.
+
+# load 'ext/spinner.rb'              # Designed for use with script/spin
+# load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
+# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
+
+set :deploy_to, "/var/www/register.rockthevote.com/rocky"
+
+role :web,  "hood.osuosl.org"
+role :app,  "hood.osuosl.org"
+role :util, "rainier.osuosl.org"
+role :db,   "hood.osuosl.org"
+
+set :scm, "git"
+set :user, "rocky"
+set :branch, "master"
+
+set :deploy_via, :remote_cache
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
