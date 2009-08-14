2e2830c2ca7bd5f09d09995c61bef88612eafa53
Alex Lobascio & Jacob Maine <ops+bosh+jacob@pivotallabs.com>
2009-08-14 16:22:22 -0700

    partner styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,7 +22,7 @@ module ApplicationHelper
     content_tag(:ul, messages.join("\n"), :class => "flash")
   end
 
-  def clippy(text, bgcolor='#FFFFFF')
+  def clippy(text, bgcolor='#EAEAEA')
     html = <<-HTML
       <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
               width="110"
