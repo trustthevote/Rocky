74037a2bc1cd1bce1df12364c9f18405afefedb8
Josh Susser & Rob Olson <ops+jsusser+rolson@pivotallabs.com>
2009-08-27 16:59:27 -0700

    extraneous artifact removal

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,2 +1,2 @@
 run "ln -nfs #{shared_path}/config/newrelic.yml #{release_path}/config/newrelic.yml"
-run "ln -nfs #{shared_path}/config/initializers/hoptoad.rb˜ #{release_path}/config/initializers/hoptoad.rb"
+run "ln -nfs #{shared_path}/config/initializers/hoptoad.rb #{release_path}/config/initializers/hoptoad.rb"
