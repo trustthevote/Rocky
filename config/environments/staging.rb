b804154cde34a01bbd0f4a76627f9cc8abfe4e72
Lance Albertson <lance@osuosl.org>
2009-09-16 10:44:58 -0700

    use staging URL

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -24,7 +24,7 @@ config.action_view.cache_template_loading            = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options = { :host => "rockthevote.com" }
+config.action_mailer.default_url_options = { :host => "rtvstaging.osuosl.org" }
 FROM_ADDRESS = "register@rockthevote.com"
 
 # Enable threaded mode
