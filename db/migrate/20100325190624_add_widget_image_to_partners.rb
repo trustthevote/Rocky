50af7cedbde0361ff60021ac5f9eae78812d887c
Glenn Jahnke & Josh Susser <pair+glennj+jsusser@pivotallabs.com>
2010-03-30 10:59:52 -0700

    set default widget image in migration. does NOT change schema from old version.

--- db/migrate/20100325190624_add_widget_image_to_partners.rb
+++ db/migrate/20100325190624_add_widget_image_to_partners.rb
@@ -1,6 +1,10 @@
 class AddWidgetImageToPartners < ActiveRecord::Migration
   def self.up
     add_column "partners", "widget_image", :string
+
+    sql = "UPDATE partners SET widget_image = 'rtv234x60v1' WHERE widget_image IS NULL"
+    how_many = ActiveRecord::Base.connection.update(sql)
+    say "set default widget image for #{how_many} partners"
   end
 
   def self.down
