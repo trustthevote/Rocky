9a6990bc0a5686844a878732bb1d13f5e9ff136c
Glenn Jahnke & Josh Susser <pair+glennj+jsusser@pivotallabs.com>
2010-03-30 11:06:21 -0700

    use filename, not image symbolic name

--- db/migrate/20100325190624_add_widget_image_to_partners.rb
+++ db/migrate/20100325190624_add_widget_image_to_partners.rb
@@ -2,7 +2,7 @@ class AddWidgetImageToPartners < ActiveRecord::Migration
   def self.up
     add_column "partners", "widget_image", :string
 
-    sql = "UPDATE partners SET widget_image = 'rtv234x60v1' WHERE widget_image IS NULL"
+    sql = "UPDATE partners SET widget_image = 'rtv-234x60-v1.gif' WHERE widget_image IS NULL"
     how_many = ActiveRecord::Base.connection.update(sql)
     say "set default widget image for #{how_many} partners"
   end
