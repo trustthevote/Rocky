7a6a3cf949a524c27ae2216b838b7be7134cb80d
Glenn Jahnke & Josh Susser <pair+glennj+jsusser@pivotallabs.com>
2010-04-30 16:52:07 -0700

    Removing clippy copy-paste widget for embed codes
    Was broken, and cause visual problems...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,33 +22,6 @@ module ApplicationHelper
     content_tag(:ul, messages.join("\n"), :class => "flash")
   end
 
-  def clippy(text, bgcolor='#dddddd')
-    html = <<-HTML
-      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
-              width="110"
-              height="14"
-              id="clippy" >
-      <param name="movie" value="/flash/clippy.swf"/>
-      <param name="allowScriptAccess" value="always" />
-      <param name="quality" value="high" />
-      <param name="scale" value="noscale" />
-      <param NAME="FlashVars" value="text=#{text}">
-      <param name="bgcolor" value="#{bgcolor}">
-      <embed src="/flash/clippy.swf"
-             width="110"
-             height="14"
-             name="clippy"
-             quality="high"
-             allowScriptAccess="always"
-             type="application/x-shockwave-flash"
-             pluginspage="http://www.macromedia.com/go/getflashplayer"
-             FlashVars="text=#{text}"
-             bgcolor="#{bgcolor}"
-      />
-      </object>
-    HTML
-  end
-
   def partner_locale_options(partner, locale, source)
     opts = {}
     opts[:partner] = partner unless partner == Partner.default_id
