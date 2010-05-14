652aa55a7ef0f831e9cff1da78ea35d2a310f85a
Josh Susser <jsusser@pivotallabs.com>
2010-05-14 10:34:11 -0700

    use floatbox 4.01 from branding gem

--- public/floatbox/licenseKey.js
+++
@@ -1,6 +0,0 @@
-/***************************************************************|
-| If you have a license key, paste it between the quotes below. |
-| To obtain a new license key, please visit                     |
-| http://randomous.com/floatbox/register                        |
-|***************************************************************/
-fb.licenseKey = "xkeHykj9v1Pb";
