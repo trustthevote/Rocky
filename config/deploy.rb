4e4aceb922b4985997cfa78cd80034e8fb09dc81
Lance Albertson <lance@osuosl.org>
2010-09-08 13:27:21 -0700

    update git repository location

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "rocky"
-set :repository,  "git@git.osuosl.org:/var/git/rocky.git"
+set :repository,  "git@git.osuosl.org:rocky"
 
 # If you have previously been relying upon the code to start, stop 
 # and restart your mongrel application, or if you rely on the database
