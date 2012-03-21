4afeab4ec5099cd6fc8c95c1ea19f41cc5022193
Alex Mekelburg <alex.mekelburg@gmail.com>
2012-03-20 22:14:20 -0400

    updates to use bundler/gemfile (and ignore some extra stuff in .gitignore)

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'http://gems.github.com'
+source 'http://gems.rubyforge.org'
+
+gem "rake", "= 0.9.2"
+gem "rails", "= 2.3.2"
+gem "aasm", "= 2.0.5"
+gem "authlogic", "= 2.1.1"
+gem "fastercsv", "= 1.5.0"
+gem "daemons", "= 1.0.10"
+gem "paperclip", "= 2.3.1.1"
+gem "sqlite3"
+
+gem "rocky_branding", "= 12.3.5"
+
+group :development, :text do
+  gem "rspec", "= 1.2.7"
+  gem "rr", "= 0.10.11"
+  gem "rspec-rails", "= 1.2.7"
+  gem "thoughtbot-factory_girl", "= 1.2.1"
+  #gem "pivotal-webrat", "= 0.4.4"
+  gem "webrat", "= 0.4.4"
+  #gem "pivotal-webrat-rspec-rails", "= 0.4.4"
+  gem "cucumber", "= 0.3.11"
+  gem "selenium-client", "= 1.2.16"
+end
+
+gem "nokogiri", "= 1.3.2"
+gem "mechanize", "= 1.0.0"
+gem "newrelic_rpm", "= 2.11.1"
+gem "hoptoad_notifier", "= 2.2.2"
--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,76 +1,20 @@
-#***** BEGIN LICENSE BLOCK *****
-#
-#Version: RTV Public License 1.0
-#
-#The contents of this file are subject to the RTV Public License Version 1.0 (the
-#"License"); you may not use this file except in compliance with the License. You
-#may obtain a copy of the License at: http://www.osdv.org/license12b/
-#
-#Software distributed under the License is distributed on an "AS IS" basis,
-#WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
-#specific language governing rights and limitations under the License.
-#
-#The Original Code is the Online Voter Registration Assistant and Partner Portal.
-#
-#The Initial Developer of the Original Code is Rock The Vote. Portions created by
-#RockTheVote are Copyright (C) RockTheVote. All Rights Reserved. The Original
-#Code contains portions Copyright [2008] Open Source Digital Voting Foundation,
-#and such portions are licensed to you under this license by Rock the Vote under
-#permission of Open Source Digital Voting Foundation.  All Rights Reserved.
-#
-#Contributor(s): Open Source Digital Voting Foundation, RockTheVote,
-#                Pivotal Labs, Oregon State University Open Source Lab.
-#
-#***** END LICENSE BLOCK *****
-############# GemInstaller Rails Preinitializer - see http://geminstaller.rubyforge.org
-
-# This file should be required in your Rails config/preinitializer.rb for Rails >= 2.0,
-# or required in config/boot.rb before initialization for Rails < 2.0.  For example:
-#   require 'geminstaller_rails_preinitializer'
-#
-# If you require a different geminstaller configuration, copy this file into your Rails app,
-# modify it, and require your customized version.  For example:
-#   require "#{File.expand_path(RAILS_ROOT)}/config/custom_geminstaller_rails_preinitializer.rb"
-
-require "rubygems" 
-require "geminstaller" 
-
-module GemInstallerRailsPreinitializer
-  class << self
-    def preinitialize
-      args = ''
-
-      # Specify --geminstaller-output=all and --rubygems-output=all for maximum debug logging
-      # args += ' --geminstaller-output=all --rubygems-output=all'
-
-      # The 'exceptions' flag determines whether errors encountered while running GemInstaller
-      # should raise exceptions (and abort Rails), or just return a nonzero return code
-      args += " --exceptions" 
-
-      # This will use sudo by default on all non-windows platforms, but requires an entry in your
-      # sudoers file to avoid having to type a password.  It can be omitted if you don't want to use sudo.
-      # See http://geminstaller.rubyforge.org/documentation/documentation.html#dealing_with_sudo
-      # Note that environment variables will NOT be passed via sudo!
-      args += " --sudo" unless RUBY_PLATFORM =~ /mswin/
-
-      # The 'install' method will auto-install gems as specified by the args and config
-      if ENV['IS_CI_BOX']
-        # you can change the output defaults under cruise if you want
-        # args += " --geminstaller-output=all --rubygems-output=all"
-
-        # Only install gems on Rails startup if we are on CI.  In local dev environment, this avoids having to type the sudo
-        # password on every app/test run, and on deployment you should be calling this before app startup as a capistrano task.
-        GemInstaller.install(args)
-      end
-
-      # The 'autogem' method will automatically add all gems in the GemInstaller config to your load path,
-      # using the rubygems 'gem' method.  Note that only the *first* version of any given gem will be loaded.
-      GemInstaller.autogem(args)
-    end
-  end
+begin
+  require "rubygems"
+  require "bundler"
+rescue LoadError
+  raise "Could not load the bundler gem. Install it with `gem install bundler`."
 end
 
-# Attempt to prevent GemInstaller from running twice, but won't work if it is executed
-# in a separate interpreter (like rake tests)
-GemInstallerRailsPreinitializer.preinitialize unless $geminstaller_initialized
-$geminstaller_initialized = true
+if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.24")
+  raise RuntimeError, "Your bundler version is too old for Rails 2.3." +
+   "Run `gem install bundler` to upgrade."
+end
+
+begin
+  # Set up load paths for all bundled gems
+  ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
+  Bundler.setup
+rescue Bundler::GemNotFound
+  raise RuntimeError, "Bundler couldn't find some gems." +
+    "Did you run `bundle install`?"
+end
