623d8d919683661c62fd0ae3717359982c0c7a2a
Aleksey Gureiev <spyromus@noizeramp.com>
2012-03-28 22:32:25 +0300

    [dev] fixing Mutext missing errors

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@
 # RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
+require 'thread'
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
