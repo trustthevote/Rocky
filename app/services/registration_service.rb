274c4b3a2cdf9a05513a06b361c2ea4ab84db334
Aleksey Gureiev <spyromus@noizeramp.com>
2012-04-05 14:25:36 +0300

    [#14] fixed handling of minimal request

--- app/services/registration_service.rb
+++ app/services/registration_service.rb
@@ -38,7 +38,7 @@ class RegistrationService
 
   # Creates a record and returns it.
   def self.create_record(data)
-    attrs = data_to_attrs(data)
+    attrs = data_to_attrs(data || {})
     reg = Registrant.build_from_api_data(attrs)
 
     if reg.save
