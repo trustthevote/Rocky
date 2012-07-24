d80901ec43f777cf2d6dff11633a9da1aa88a952
Alex Mekelburg <alex.mekelburg@gmail.com>
2012-07-23 21:51:06 -0400

    include the partner model to not generate extra queries

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -274,7 +274,7 @@ class Partner < ActiveRecord::Base
   def generate_registrants_csv
     FasterCSV.generate do |csv|
       csv << Registrant::CSV_HEADER
-      registrants.all(:include => [:home_state, :mailing_state]).each do |reg|
+      registrants.all(:include => [:home_state, :mailing_state, :partner]).each do |reg|
         csv << reg.to_csv_array
       end
     end
