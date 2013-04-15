ad5785900d7ad29e0428e84e4e6d56471d9a476c
Alexander Mekelburg <alexmek@alexander-mekelburgs-computer.local>
2013-04-14 23:47:42 -0400

    dj upgrade, rspec upgrade

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,85 +1,32 @@
-#***** BEGIN LICENSE BLOCK *****
-#
-#Version: RTV Public License 1.0
-#
-#The contents of this file are subject to the RTV Public License Version 1.0 (the
-#"License"); you may not use this file except in compliance with the License. You
-#may obtain a copy of the License at: http://www.osdv.org/license12b/
-#
-#Software distributed under the License is distributed on an "AS IS" basis,
-#WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
-#specific language governing rights and limitations under the License.
-#
-#The Original Code is the Online Voter Registration Assistant and Partner Portal.
-#
-#The Initial Developer of the Original Code is Rock The Vote. Portions created by
-#RockTheVote are Copyright (C) RockTheVote. All Rights Reserved. The Original
-#Code contains portions Copyright [2008] Open Source Digital Voting Foundation,
-#and such portions are licensed to you under this license by Rock the Vote under
-#permission of Open Source Digital Voting Foundation.  All Rights Reserved.
-#
-#Contributor(s): Open Source Digital Voting Foundation, RockTheVote,
-#                Pivotal Labs, Oregon State University Open Source Lab.
-#
-#***** END LICENSE BLOCK *****
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require File.dirname(__FILE__) + "/../config/environment" unless defined?(Rails.root)
-require 'spec/autorun'
-require 'spec/rails'
-require 'authlogic/test_case'
-require 'nokogiri'
-require 'rr'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-Spec::Runner.configure do |config|
-  # If you're not using ActiveRecord you should remove these
-  # lines, delete config/database.yml and disable :active_record
-  # in your config/boot.rb
-  config.use_transactional_fixtures = true
-  config.use_instantiated_fixtures  = false
-  config.fixture_path = Rails.root + '/spec/fixtures/'
-
-  # == Fixtures
+RSpec.configure do |config|
+  # ## Mock Framework
   #
-  # You can declare fixtures for each example_group like this:
-  #   describe "...." do
-  #     fixtures :table_a, :table_b
-  #
-  # Alternatively, if you prefer to declare them only once, you can
-  # do so right here. Just uncomment the next line and replace the fixture
-  # names with your fixtures.
-  #
-  config.global_fixtures = :all
-  #
-  # If you declare global fixtures, be aware that they will be declared
-  # for all of your examples, even those that don't use them.
-  #
-  # You can also declare which fixtures to use (for example fixtures for test/fixtures):
-  #
-  # config.fixture_path = Rails.root + '/spec/fixtures/'
-  #
-  # == Mock Framework
-  #
-  # RSpec uses it's own mocking framework by default. If you prefer to
-  # use mocha, flexmock or RR, uncomment the appropriate line:
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
   #
   # config.mock_with :mocha
   # config.mock_with :flexmock
-  config.mock_with RR::Adapters::Rspec
+  # config.mock_with :rr
 
-  # == Notes
-  #
-  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
-end
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
 
-module HoptoadNotifier
-  def self.notify(thing)
-    # do nothing.
-  end
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
 end
