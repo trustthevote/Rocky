8895b6dd44a0c90b71d381678b56fbca9e0dba67
Alex Mekelburg <alex.mekelburg@gmail.com>
2013-07-14 19:03:07 -0400

    fix NV state custom code

--- app/models/state_customizations/nv.rb
+++ app/models/state_customizations/nv.rb
@@ -1,4 +1,4 @@
-class WA < StateCustomization
+class NV < StateCustomization
   def online_reg_url(registrant)
     root_url ="https://nvsos.gov/sosvoterservices/Registration/step1.aspx?source=rtv&utm_source=rtv&utm_medium=rtv&utm_campaign=rtv"
     return root_url if registrant.nil?
