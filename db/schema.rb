eb0dfa56ac70e8758c174d4cf196df65767c177e
Alex Mekelburg <alex.mekelburg@gmail.com>
2014-01-23 08:07:39 -0500

    changed column length limit on locales

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131210193029) do
+ActiveRecord::Schema.define(:version => 20140123130350) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -106,7 +106,7 @@ ActiveRecord::Schema.define(:version => 20131210193029) do
 
   create_table "registrants", :force => true do |t|
     t.string   "status"
-    t.string   "locale",                             :limit => 2
+    t.string   "locale",                             :limit => 64
     t.integer  "partner_id"
     t.string   "uid"
     t.integer  "reminders_left",                                   :default => 0
