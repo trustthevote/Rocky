47d4a3125c9421f36e962938acde416d442e8637
Alex Mekelburg <alex.mekelburg@gmail.com>
2014-05-14 10:25:43 -0400

    staging deploys from master

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -29,6 +29,6 @@ role :db,   ENV['STAGING_DB'], :primary => true
 
 set :rails_env,    "staging"
 
-set :branch, "ovr_workflow"
+set :branch, "master"
 
 after "deploy:migrate", "deploy:symlink_translations"
