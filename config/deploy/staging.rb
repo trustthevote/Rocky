b25dbf3040b2fb7c7d179080afa4417dfdbee65f
Alex Mekelburg <alex.mekelburg@gmail.com>
2014-09-09 16:08:34 -0400

    deploy assets to util for PDF gen

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -29,6 +29,6 @@ role :db,   ENV['STAGING_DB'], :primary => true
 
 set :rails_env,    "staging"
 
-set :branch, "master"
+set :branch, "util_pdf_gen"
 
 after "deploy:migrate", "deploy:symlink_translations"
