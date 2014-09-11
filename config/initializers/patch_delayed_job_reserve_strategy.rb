a1c7246116176a6d4dffd38b2ceabd0f0228a65d
Alex Mekelburg <alex.mekelburg@gmail.com>
2014-09-11 09:59:00 -0400

    more patching

--- config/initializers/patch_delayed_job_reserve_strategy.rb
+++ config/initializers/patch_delayed_job_reserve_strategy.rb
@@ -7,16 +7,16 @@ module Delayed
         # override UPDATE..LIMIT strategy
         # see https://github.com/collectiveidea/delayed_job_active_record/issues/63#issuecomment-26284690
         def self.reserve(worker, max_run_time = Worker.max_run_time)
-          # if Worker.queues == ['test']
-          #
-          #   now = self.db_time_now
-          #   job = self.where({
-          #     :locked_at=>nil,
-          #     :queue=>'test'
-          #   }).lock(true).first
-          #   job.update_attributes({:locked_at => now, :locked_by => worker.name})
-          #   job.reload
-          # else
+          if Worker.queues == ['test']
+            
+            now = self.db_time_now
+            job = self.where({
+              :locked_at=>nil,
+              :queue=>'test'
+            }).lock(true).first
+            job.update_attributes({:locked_at => now, :locked_by => worker.name})
+            job.reload
+          else
             ready_scope = self.ready_to_run(worker.name, max_run_time)
             ready_scope = ready_scope.where('priority >= ?', Worker.min_priority) if Worker.min_priority
             ready_scope = ready_scope.where('priority <= ?', Worker.max_priority) if Worker.max_priority
@@ -28,7 +28,7 @@ module Delayed
               count = ready_scope.where(:id => job.id).update_all(:locked_at => now, :locked_by => worker.name)
               count == 1 && job.reload
             end
-          # end
+          end
         end
       end
     end
