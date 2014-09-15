ce1524f24b40a2e4e5dd594ef1ec2c5008f93431
Alex Mekelburg <alex.mekelburg@gmail.com>
2014-09-15 17:43:13 -0400

    better AbandonStaleRecords initial query

--- app/models/registrant.rb
+++ app/models/registrant.rb
@@ -423,7 +423,8 @@ class Registrant < ActiveRecord::Base
   end
 
   def self.abandon_stale_records
-    self.find_each(:batch_size=>500, :conditions => ["(abandoned != ?) AND (status != 'complete') AND (updated_at < ?)", true, STALE_TIMEOUT.seconds.ago]) do |reg|
+    id_list = self.where("(abandoned != ?) AND (status != 'complete') AND (updated_at < ?)", true, STALE_TIMEOUT.seconds.ago).pluck(:id)
+    self.find_each(:batch_size=>500, :conditions => ["id in (?)", id_list]) do |reg|
       if reg.finish_with_state?
         reg.status = "complete"
         begin
