d4a7c80a2e7aec8d78edd8024ad06537239b79b8
Alex Mekelburg <alex.mekelburg@gmail.com>
2014-09-17 10:21:41 -0400

    # of workers

--- config/deploy.rb
+++ config/deploy.rb
@@ -241,7 +241,7 @@ namespace :deploy do
   task :run_pdf_workers, :roles => :pdf do
     run "cd #{latest_release} && RAILS_ENV=#{rails_env} bundle exec ruby script/rocky_pdf_runner stop"
     sleep 5
-    2.times do
+    12.times do
       run "cd #{latest_release} && RAILS_ENV=#{rails_env} TZ=:/etc/localtime bundle exec ruby script/rocky_pdf_runner start"
     end
   end
