a9a51bc71773e617114d02470e198f48d9faffbd
Alex Mekelburg <alex.mekelburg@gmail.com>
2014-11-20 11:39:01 -0500

    don't init wickedPDF on non-pdf servers'

--- config/initializers/wicked_pdf_config.rb
+++ config/initializers/wicked_pdf_config.rb
@@ -1,4 +1,4 @@
-unless Rails.env.development? || Rails.env.test?
+unless Rails.env.development? || Rails.env.test? || ENV['NO_PDFS']
   WickedPdf.config = {
     :exe_path => '/var/www/register.rockthevote.com/rocky/shared/bundle/ruby/1.9.1/bin/wkhtmltopdf'
   }
