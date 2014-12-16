77838647404281cd9664886e8c1b270c54291eff
Alex Mekelburg <alex.mekelburg@gmail.com>
2014-12-16 15:10:30 -0500

    use proximo proxy

--- app/models/state_customizations/ca.rb
+++ app/models/state_customizations/ca.rb
@@ -334,6 +334,7 @@ class CA < StateCustomization
       NUM_DISCLOSURES.times do |i|
         num = i+1
         begin
+          RestClient.proxy = ENV["PROXIMO_URL"]
           @@disclosures[locale.to_s][num] = RestClient.get(disclosure_url(locale, num)).to_s.force_encoding('UTF-8')
         rescue Exception=>e
           log_covr_error("While loading disclosures from #{disclosure_url(locale, num)} - #{e.message}\n#{e.backtrace.join("\n\t")}")
--- lib/integrations/soap.rb
+++ lib/integrations/soap.rb
@@ -5,6 +5,7 @@ module Integrations
       if Rails.env.development?
         return File.new(Rails.root.join("spec/fixtures/files/covr/max_registrant_response.xml")).read
       else
+        RestClient.proxy = ENV["PROXIMO_URL"]
         return RestClient::Request.execute(:method=>:post, :payload=>xml_content, :url=>url, :headers=>{:content_type=>"application/soap+xml;charset=UTF-8"}, :timeout=>17, :open_timeout=>3)
         #return RestClient.post(url, xml_content, :content_type=>"application/soap+xml;charset=UTF-8", :timeout => 15)
       end
