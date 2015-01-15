ecbbb9a9a498848d064cbee18ec9c47ae989eb46
Alex Mekelburg <alex.mekelburg@gmail.com>
2015-01-15 06:32:25 -0500

    expire caches

--- config/initializers/active_resource_caching.rb
+++ config/initializers/active_resource_caching.rb
@@ -56,6 +56,7 @@ else
                        :password => ENV["MEMCACHIER_PASSWORD"],
                        :failover => true,
                        :socket_timeout => 1.5,
-                       :socket_failure_delay => 0.2
+                       :socket_failure_delay => 0.2,
+                       :expires_in => 5.minutes
                       }
 end
