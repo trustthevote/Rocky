40207f96e0e30d389f149eefeae6a11e4406d9ba
Alex Mekelburg <alex.mekelburg@gmail.com>
2015-02-02 16:49:05 -0500

    fix wicked pdf config to allow for missing env name

--- config/initializers/wicked_pdf_config.rb
+++ config/initializers/wicked_pdf_config.rb
@@ -1,4 +1,4 @@
-if !Rails.env.development? && !Rails.env.test? && (ENV['ROCKY_ROLE'].downcase=='pdfgen' || ENV['ROCKY_ROLE'].downcase=='util')
+if !Rails.env.development? && !Rails.env.test? && (ENV['ROCKY_ROLE'].to_s.downcase=='pdfgen' || ENV['ROCKY_ROLE'].to_s.downcase=='util')
   WickedPdf.config = {
     :exe_path => '/var/www/register.rockthevote.com/rocky/shared/bundle/ruby/1.9.1/bin/wkhtmltopdf'
   }
